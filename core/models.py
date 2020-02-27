1e8c1debdd26342eb28ec662f4717b6442c72c50
mack66hm <mack66hm@gmail.com>
2020-02-27 17:17:33 -0500

    created model

--- core/models.py
+++ core/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+    title = models.CharField(max_length= 500)
+    author = models.CharField(max_length= 500)
+    description = models.TextField(max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Book title: {self.title} description: {self.description}'
