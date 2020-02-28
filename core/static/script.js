57abf7434fb5ee38bcfee850dedb571b42488580
dandeboer <ddeboer926@gmail.com>
2020-02-28 14:24:26 -0500

    javascript file now has a working function that will sort the titles

--- core/static/script.js
+++ core/static/script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+let bookSelector = document.querySelectorAll(".book")
+let bookSorter = []
+let title = 0
+
+function test() {
+    for (let book of bookSelector) {
+        bookSorter.push(book.firstElementChild.innerText)
+        bookSorter.sort(function (a, b) {
+            return a.toLowerCase().localeCompare(b.toLowerCase())
+        })
+    }
+    for (let book of bookSelector) {
+        book.firstElementChild.innerText = bookSorter[title]
+        title += 1
+    }
+}
+
+test()
