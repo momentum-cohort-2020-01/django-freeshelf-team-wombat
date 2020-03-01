72a08d31ecb4c6a6c52d2f9b853960c961722811
dandeboer <ddeboer926@gmail.com>
2020-03-01 18:55:31 -0500

    commiting before i break something

--- core/static/script.js
+++ core/static/script.js
@@ -6,46 +6,45 @@ let urlSelector = document.querySelectorAll(".url")
 let categorySelector = document.querySelectorAll(".category")
 let addedSelector = document.querySelectorAll(".added")
 
-
+const defaultSorter = []
 let bookSorter = []
 let num = 0
 
-function sortAlphabetical(arg) {
+function sorter(arg) {
     num = 0
     for (let book of bookSelector) {
-        bookSorter.push({title: "", author: "", description: "", url: ""})
+        arg.push({title: "", author: "", description: "", url: ""})
     }
     for (let title of titleSelector) {
-        bookSorter[num]["title"] = title.innerText
+        arg[num]["title"] = title.innerText
         num += 1
     }
     num = 0
     for (let author of authorSelector) {
-        bookSorter[num]["author"] = author.innerText
+        arg[num]["author"] = author.innerText
         num += 1
     }
     num = 0
     for (let description of descriptionSelector) {
-        bookSorter[num]["description"] = description.innerText
+        arg[num]["description"] = description.innerText
         num += 1
     }
     num = 0
     for (let url of urlSelector) {
-        bookSorter[num]["url"] = url.innerText
+        arg[num]["url"] = url.innerText
         num += 1
     }
     num = 0
     for (let category of categorySelector) {
-        bookSorter[num]["category"] = category.innerText
+        arg[num]["category"] = category.innerText
         num += 1
     }
     num = 0
     for (let added of addedSelector) {
-        bookSorter[num]["added"] = added.innerText
+        arg[num]["added"] = added.innerText
         num += 1
     }
-
-    bookSorter.sort(arg)
+    // bookSorter.sort(arg)
     // booksToDom()
 }
 
@@ -69,36 +68,45 @@ function reverseAlphatical(a, b) {
     return 0
 }
 
-function booksToDom() {
+function alphabetize (arg) {
+    bookSorter = []
+    sorter(bookSorter)
+    bookSorter.sort(arg)
+    booksToDom(bookSorter)
+}
+
+function booksToDom(arg) {
     num = 0
     for (let title of titleSelector) {
-        title.innerText = bookSorter[num]['title']
+        title.innerText = arg[num]['title']
         num += 1
     }
     num = 0
     for (let author of authorSelector) {
-        author.innerText = bookSorter[num]['author']
+        author.innerText = arg[num]['author']
         num += 1
     }
     num = 0
     for (let description of descriptionSelector) {
-        description.innerText = bookSorter[num]['description']
+        description.innerText = arg[num]['description']
         num += 1
     }
     num = 0
     for (let url of urlSelector) {
-        url.innerText = bookSorter[num]['url']
-        url.setAttribute("href", bookSorter[num]['url'])
+        url.innerText = arg[num]['url']
+        url.setAttribute("href", arg[num]['url'])
         num += 1
     }
     num = 0
     for (let category of categorySelector) {
-        category.innerText = bookSorter[num]['category']
+        category.innerText = arg[num]['category']
         num += 1
     }
     num = 0
     for (let added of addedSelector) {
-        added.innerText = bookSorter[num]['added']
+        added.innerText = arg[num]['added']
         num += 1
     }
 }
+
+sorter(defaultSorter)
