53e4157eb2a05a89e5aa2e5b6f1298a000be8cb7
mack66hm <mack66hm@gmail.com>
2020-03-02 11:47:06 -0500

    created user

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_requried
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
